6905447caf7af71aba169a577b22c66dbe8ace08
youngsil <youngsil0128@seowoninfo.com>
2024-01-18 13:44:57 +0900

    날씨부분 수정
    오타 수정

--- momentum/js/weather.js
+++ momentum/js/weather.js
@@ -5,28 +5,31 @@ const API_KEY ="13e649d29055d7aa4998165447c1011f"
 function onGeoOk (position){
     const lat = position.coords.latitude; // 위도
     const lon = position.coords.longitude; // 경도
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&unit=metric`
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+                //https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric
+                //https://api.openweathermap.org/data/3.0/onecall?lat=37.4940135&lon=127.028875&appid=13e649d29055d7aa4998165447c1011f
+    
+                console.log(url);
 
     fetch(url)
-    .then((respinse) => respinse.json())
-    .them((data) => {
-        // const weather = document.querySelector("#wearher span:first-child");
-        // const city = document.querySelector("#wearher span:last-child");
+    .then((response) => response.json())
+    .then((data) => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
 
-        // const weather = data.name;
-        // const city = data.weather[0].main;
-        city.innerText = data.name; 
+        city.innerText = data.name;
         weather.innerText = `${data.weather[0].main}/${data.main.temp}`
 
-    }); // 자바스크립트가 api를 부름
-
- 
+    }); 
+    // 자바스크립트가 api를 부름
+    // https://api.openweathermap.org/data/3.0/onecall?lat=37.4940135&lon=127.028875&appid=13e649d29055d7aa4998165447c1011f
+    // https://api.openweathermap.org/data/3.0/onecall?lat=4940135&lon=127.028875&exclude={part}&appid={API key}
     // https://api.openweathermap.org/data/2.5/weather?lat=4940135&lon=127.028875&appid=13e649d29055d7aa4998165447c1011f
 
     console.log(" 당신은 여기에 있군요", lat, lon);
     //37.4940135 127.028875
     // 13e649d29055d7aa4998165447c1011f
-    console.lig(url);
+    
     
 }
 
