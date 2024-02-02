9576b1ad42eea0b5d9256650f03e758f588e2094
youngsil <youngsil0128@seowoninfo.com>
2024-02-02 11:25:59 +0900

    챌린지 수정
    css 수정

--- momentum/js/greeting.js
+++ momentum/js/greeting.js
@@ -1,93 +1,3 @@
-
-
-// const h1 = document.querySelector("div.hello:first-child h1") // querySelector css 방식으로 찾음 
-// // const titleA = document.querySelectorAll(".hello h1")
-
-// function handleTitleClick () {
-//     // const currentColor =  h1.style.color;
-//     // let newColor;
-//     // // console.log(h1.style.color);
-//     // // h1.style.color = "blue";
-//     // //console.log(h1.style.color);
-//     // if ( currentColor === "blue" ) {
-//     //    newColor = "tomato";
-//     // } else {
-//     //    newColor = "blue"
-//     // }
-//     // h1.style.color = newColor;
-//     // h1.className = "active";
-//     //const clickClass = "active"
-//     // if (h1.classList.contains(clickClass)) {
-//     //      h1.classList.remove(clickClass);
-//     //     } else {
-//     //     h1.classList.add(clickClass)
-//     // }
-//     h1.classList.toggle("active");
-// }
-
-// h1.addEventListener("click", handleTitleClick);
-// const calculator = {
-//     add : function (a,b) {
-//         console.log (a + b);
-//     },
-//     minus: function (a,b){
-//         console.log (a - b);
-//     },
-//     div: function (a,b) {
-//         console.log (a / b);
-//     },
-//     multi: function (a,b){
-//         console.log (a * b);
-//     },
-//     power: function (a,b){
-//         console.log (a ** b);
-//     }
-
-// }
-// calculator.minus (2 , 2);
-// calculator.div (2 , 2);
-// calculator.multi (2 , 2);
-// calculator.power (2 , 2);
-
-// const age = 32;
-// function calculateKrAge (ageOfForeigner) {
-//     return ageOfForeigner + 2; 
-// }
-
-// const KrAge = calculateKrAge(age);
-// console.log(KrAge)
-
-//parseInt() type을 number로 변환시켜줌 
-// const age = parseInt(prompt("how old are you?"));
-    
-//     console.log(isNaN(age));
-    // or 
-    // true || true === true
-    // false || true === true
-    // true || false === true
-    // false || false === false
-    
-    // and 
-    // true && true === true
-    // true && false === false
-    // false && true === false
-    // true && false === false
-
-    // if ( isNaN(age) || age < 0) { //  || 하나만 맞아도  true
-    //     alert("양수만 입력하세요");
-    // } else if ( age < 18) {
-    //     alert("미성년자는 음주를 삼가주세요");
-    // } else if ( age >= 18 && age <= 50) { // && 둘 다 맞아야 true 
-    //     alert("성인입니다");
-    // } else {
-    //     alert("건강을 위해 음주를 삼가주세요 ");
-    // }
-
-
-
-
-
-
 // const loginForm = document.getElementById("login-form");
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
@@ -111,7 +21,7 @@ function onLoginSubmit (event) {
 
 function paintGreetings(){
     const username = localStorage.getItem((USERNAME_KEY));
-    greeting.innerHTML = `hello ${username}`;
+    greeting.innerHTML = `HELLO ${username}!`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
@@ -131,5 +41,5 @@ else {
     paintGreetings();
     
 }
- 
+
 
--- momentum/js/weather.js
+++ momentum/js/weather.js
@@ -1,15 +1,9 @@
-
-
 const API_KEY ="13e649d29055d7aa4998165447c1011f"
 
 function onGeoOk (position){
     const lat = position.coords.latitude; // 위도
     const lon = position.coords.longitude; // 경도
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
-                //https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric
-                //https://api.openweathermap.org/data/3.0/onecall?lat=37.4940135&lon=127.028875&appid=13e649d29055d7aa4998165447c1011f
-    
-                console.log(url);
 
     fetch(url)
     .then((response) => response.json())
@@ -22,14 +16,6 @@ function onGeoOk (position){
 
     }); 
     // 자바스크립트가 api를 부름
-    // https://api.openweathermap.org/data/3.0/onecall?lat=37.4940135&lon=127.028875&appid=13e649d29055d7aa4998165447c1011f
-    // https://api.openweathermap.org/data/3.0/onecall?lat=4940135&lon=127.028875&exclude={part}&appid={API key}
-    // https://api.openweathermap.org/data/2.5/weather?lat=4940135&lon=127.028875&appid=13e649d29055d7aa4998165447c1011f
-
-    console.log(" 당신은 여기에 있군요", lat, lon);
-    //37.4940135 127.028875
-    // 13e649d29055d7aa4998165447c1011f
-    
     
 }
 
